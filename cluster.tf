9cf449d888ac76956fe946404e71380b835769a9
Sushma <sushma.gayatri451@gmail.com>
2022-11-28 20:45:42 +0530

    db

--- cluster.tf
+++ cluster.tf
@@ -1,12 +1,13 @@
-# # Creates Docdb Cluster
+ # Creates Docdb Cluster
 
-# resource "aws_docdb_cluster" "docdb" {
-#   cluster_identifier      = "roboshop-${var.ENV}"
-#   engine                  = "docdb"
-#   master_username         = "admin1"
-#   master_password         = "roboshop1"
-#   skip_final_snapshot     = false # True only during lab in prod, we will ytake a snapshot and that time this will be true
-# }
+resource "aws_docdb_cluster" "docdb" {
+  cluster_identifier      = "roboshop-${var.ENV}"
+   engine                  = "docdb"
+  master_username         = "admin1"
+  master_password         = "roboshop1"
+  skip_final_snapshot     = false # True only during lab in prod, we will ytake a snapshot and that time this will be true
+  db_subnet_group_name    = aws_docdb_subnet_group.docdb.name
+}
 
 # Creates subnet group
 
@@ -18,3 +19,19 @@ resource "aws_docdb_subnet_group" "docdb" {
     Name = "My docdb subnet group"
   }
 }
+
+# Creates DocDB cluster instance and adds then to the cluster
+
+resource "aws_docdb_cluster_instance" "cluster_instances" {
+  count              = 1
+  identifier         = "roboshop-${var.ENV}"
+  cluster_identifier = aws_docdb_cluster.docdb.id
+  instance_class     = "db.t3.medium"
+}
+
+# resource "aws_docdb_cluster" "default" {
+#   cluster_identifier = "docdb-cluster-demo"
+#   availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
+#   master_username    = "foo"
+#   master_password    = "barbut8chars"
+# }
