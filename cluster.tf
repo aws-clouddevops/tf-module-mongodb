823dc52cf4344c3dcb05ce66d75a1e1a28f0e051
Sushma <sushma.gayatri451@gmail.com>
2022-11-28 12:26:42 +0530

    db

--- cluster.tf
+++ cluster.tf
@@ -1,18 +1,18 @@
-# Creates Docdb Cluster
+# # Creates Docdb Cluster
 
-resource "aws_docdb_cluster" "docdb" {
-  cluster_identifier      = "roboshop-${var.ENV}"
-  engine                  = "docdb"
-  master_username         = "admin1"
-  master_password         = "roboshop1"
-  skip_final_snapshot     = false # True only during lab in prod, we will ytake a snapshot and that time this will be true
-}
+# resource "aws_docdb_cluster" "docdb" {
+#   cluster_identifier      = "roboshop-${var.ENV}"
+#   engine                  = "docdb"
+#   master_username         = "admin1"
+#   master_password         = "roboshop1"
+#   skip_final_snapshot     = false # True only during lab in prod, we will ytake a snapshot and that time this will be true
+# }
 
 # Creates subnet group
 
 resource "aws_docdb_subnet_group" "default" {
   name       = "roboshop-${var.ENV}"
-  subnet_ids = [aws_subnet.frontend.id, aws_subnet.backend.id]
+  subnet_ids =  data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
 
   tags = {
     Name = "My docdb subnet group"
