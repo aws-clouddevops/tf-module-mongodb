8583aadb9215ce8d4a915a273caa24af51210ebd
Sushma <sushma.gayatri451@gmail.com>
2022-12-05 18:13:04 +0530

    mongo

--- cluster.tf
+++ cluster.tf
@@ -2,7 +2,7 @@
 
 resource "aws_docdb_cluster" "docdb" {
   cluster_identifier      = "roboshop-${var.ENV}"
-   engine                  = "docdb"
+  engine                  = "docdb"
   master_username         = "admin1"
   master_password         = "roboshop1"
   skip_final_snapshot     = true # True only during lab in prod, we will take a snapshot and that time this will be False
