8cbc25ca475a370d622a9cf009abb891b866f562
Sushma <sushma.gayatri451@gmail.com>
2022-12-05 08:29:09 +0530

    docdb

--- cluster.tf
+++ cluster.tf
@@ -10,22 +10,6 @@ resource "aws_docdb_cluster" "docdb" {
   vpc_security_group_ids  = [aws_security_group.allow_docdb.id]
 }
 
- resource "null_resource" "mongodb-schema" {
-   depends_on = [aws_db_instance.docdb]
-  
-    provisioner "local-exec" {
-     command = <<EOF
-    cd /tmp/
-    curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"   unzip mysql-main
-    wget https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem
-    unzip -o mongodb.zip
-    cd mongodb-main
-    mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint}:27017 --sslCAFile /tmp/rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < catalogue.js
-    mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint}:27017 --sslCAFile /tmp/rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < users.js
-   EOF
-   }
- }
-
 # Creates subnet group
 
 resource "aws_docdb_subnet_group" "docdb" {
